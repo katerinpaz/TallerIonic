e42f57317eaf88cf78a6c0995b60563af356cb18
faster27 <dcorrealenis@gmail.com>
2021-10-25 19:22:15 -0500

    login con mensaje

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,53 @@
 import { Component } from '@angular/core';
+import { AlertController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
+
+
 export class Tab1Page {
 
-  constructor() {}
+
+  usuario= "daniel";
+  contrasena="1234";
+
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
+
+
+
+  }
+
+  login(){
+    
+      if(this.usuario==='daniel' && this.contrasena==='1234'){
+
+      
+        
+        this.navCtrl.navigateRoot(['tabs/tab2']);
+
+      }else{
+
+        this.presentAlert();
+
+
+      }
+
+
+  }
+
+  async presentAlert() {
+    let alert = await this.alertCtrl.create({
+     
+  
+      buttons: ['aceptar']
+    });
+    alert.present();
+  }
 
 }
+
+
